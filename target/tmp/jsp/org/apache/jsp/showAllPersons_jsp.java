621c7981e17451c517dc8e965eb04a03a941dec3
AChlebba <AChlebba@univ.gda.pl>
2015-11-25 08:48:31 +0100

    JSTL

--- target/tmp/jsp/org/apache/jsp/showAllPersons_jsp.java
+++ target/tmp/jsp/org/apache/jsp/showAllPersons_jsp.java
@@ -12,10 +12,23 @@ public final class showAllPersons_jsp extends org.apache.jasper.runtime.HttpJspB
 
   private static java.util.Vector _jspx_dependants;
 
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_forTokens_var_items_delims;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_out_value_nobody;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public Object getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_forTokens_var_items_delims = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_c_out_value_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_forTokens_var_items_delims.release();
+    _jspx_tagPool_c_out_value_nobody.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -43,6 +56,7 @@ public final class showAllPersons_jsp extends org.apache.jasper.runtime.HttpJspB
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
       out.write("\n");
+      out.write(" \n");
       out.write("\n");
       out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
       out.write("<html>\n");
@@ -69,13 +83,22 @@ public final class showAllPersons_jsp extends org.apache.jasper.runtime.HttpJspB
           _jspx_page_context.setAttribute("perso", perso, PageContext.SESSION_SCOPE);
         }
       }
-      out.write('\n');
-      out.write('\n');
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      if (_jspx_meth_c_forTokens_0(_jspx_page_context))
+        return;
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
 
   for (Person person : storage.getAllPersons()) {
 	  out.println("<p>Imie: " + person.getFirstName() + "; Rok urodzenia: " + person.getYob() + "; Przedmiot: " + person.getZakup() + "; Ilosc: " + person.getIlosc() + "; Do zaplaty : " + person.getCena() + "</p>");
   }
 
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
       out.write("\n");
       out.write("<form action=\"removePerson.jsp\">\n");
       out.write("\n");
@@ -103,5 +126,63 @@ public final class showAllPersons_jsp extends org.apache.jasper.runtime.HttpJspB
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
+  }
+
+  private boolean _jspx_meth_c_forTokens_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:forTokens
+    org.apache.taglibs.standard.tag.el.core.ForTokensTag _jspx_th_c_forTokens_0 = (org.apache.taglibs.standard.tag.el.core.ForTokensTag) _jspx_tagPool_c_forTokens_var_items_delims.get(org.apache.taglibs.standard.tag.el.core.ForTokensTag.class);
+    _jspx_th_c_forTokens_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_forTokens_0.setParent(null);
+    _jspx_th_c_forTokens_0.setItems("Imie:,Rok urodzenia:,Przedmiot:,Ilosc:,Do zaplaty :");
+    _jspx_th_c_forTokens_0.setDelims(",");
+    _jspx_th_c_forTokens_0.setVar("name");
+    int[] _jspx_push_body_count_c_forTokens_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_forTokens_0 = _jspx_th_c_forTokens_0.doStartTag();
+      if (_jspx_eval_c_forTokens_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write("\n");
+          out.write("   ");
+          if (_jspx_meth_c_out_0((javax.servlet.jsp.tagext.JspTag) _jspx_th_c_forTokens_0, _jspx_page_context, _jspx_push_body_count_c_forTokens_0))
+            return true;
+          out.write('\n');
+          int evalDoAfterBody = _jspx_th_c_forTokens_0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_c_forTokens_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_forTokens_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_forTokens_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_forTokens_0.doFinally();
+      _jspx_tagPool_c_forTokens_var_items_delims.reuse(_jspx_th_c_forTokens_0);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_c_out_0(javax.servlet.jsp.tagext.JspTag _jspx_th_c_forTokens_0, PageContext _jspx_page_context, int[] _jspx_push_body_count_c_forTokens_0)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:out
+    org.apache.taglibs.standard.tag.el.core.OutTag _jspx_th_c_out_0 = (org.apache.taglibs.standard.tag.el.core.OutTag) _jspx_tagPool_c_out_value_nobody.get(org.apache.taglibs.standard.tag.el.core.OutTag.class);
+    _jspx_th_c_out_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_out_0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_forTokens_0);
+    _jspx_th_c_out_0.setValue("${name}");
+    int _jspx_eval_c_out_0 = _jspx_th_c_out_0.doStartTag();
+    if (_jspx_th_c_out_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_0);
+      return true;
+    }
+    _jspx_tagPool_c_out_value_nobody.reuse(_jspx_th_c_out_0);
+    return false;
   }
 }
